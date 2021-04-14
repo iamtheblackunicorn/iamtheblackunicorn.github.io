d5383954d7e0df9c7ae9b0a2edc9889fc167e690
iamtheblackunicorn <drkuncrn@protonmail.ch>
2021-04-14 21:03:03 +0200

    more functionality

--- assets/javascript/nav.js
+++ assets/javascript/nav.js
@@ -1,16 +1,8 @@
 /// This function handles navigation opening and closing.
 function navHandler(){
-  let navElement = document.getElementById(
-    'nav'
-  );
-  let navElementDisplay = window.getComputedStyle(
-    navElement, null
-  ).getPropertyValue(
-    'display'
-  );
-  if (
-    navElementDisplay == 'none'
-  ) {
+  let navElement = document.getElementById('nav');
+  let navElementDisplay = window.getComputedStyle(navElement, null).getPropertyValue('display');
+  if (navElementDisplay == 'none') {
     document.getElementById('nav').style.display = 'block';
   } else {
     document.getElementById('nav').style.display = 'none';
